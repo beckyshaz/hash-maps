aaa12fc6f04527ff5393ecaa41b1e8886a2662be
beckyshaz <sharonsakah8@gmail.com>
2026-02-11 20:08:33 +0300

    created a hashmap with a hash function that takes a key and hashes it to give the location of the key in the array, also added methods like set that sets a key with its value in the hashmap and get that gets the value of the entered key and has that returns true if the key is in the hash map and false otherwise and a remove key that removes they key value pair of the key passed to it as argument

---
+++ index.js
@@ -0,0 +1,120 @@
+class HashMap{
+    constructor() {
+        this.loadFactor = 0.75;
+
+        this.capacity = 16;
+
+        this.buckets = new Array(this.capacity).fill().map(() => []);
+    }
+
+    hash(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+          hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+        }
+      
+        return hashCode;
+      } 
+
+
+      set(key, value) {
+        const  bucktLocation = this.hash(key);
+
+        const buckets = this.buckets;
+
+        const bucket = buckets[bucktLocation];
+
+        for (const i of  bucket) {
+            if (i.key === key) {
+                i.value = value;
+                return;
+            }
+        }
+
+        bucket.push({key, value});
+        return value;
+      }
+
+
+      doubleLoadFactor() {
+        let exeeded = this.loadFactor * this.capacity;
+        if (this.capacity > exeeded) {
+          this.capacity * 2;
+        }
+
+        const previousArray = this.buckets;
+
+        this.buckets = new Array(this.capacity).fill().map(() => []);
+
+
+
+      }
+
+      //get(key) takes one argument as a key and returns the value that is assigned to this key.
+      // If a key is not found, return null.
+
+      get(key) {
+        const bucktLocation = this.hash(key);
+
+        const bucket = this.buckets[bucktLocation];
+
+        for (const entry of bucket) {
+          if (key === entry.key) {
+            return entry.value;
+          }
+
+        }
+        return null;
+
+      }
+
+      //has(key) takes a key as an argument and returns true or false based on whether
+      // or not the key is in the hash map.
+
+      has(key) {
+        const bucketLocation = this.hash(key);
+
+        const bucket = this.buckets[bucketLocation];
+
+        for (let i = 0; i < bucket.length; i++) {
+          if(bucket[i].key === key) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+      //remove(key) takes a key as an argument. If the given key is in the hash map,
+      // it should remove the entry with that key and return true.
+      // If the key isn’t in the hash map, it should return false.
+
+
+      remove(key) {
+        const bucketLocation = hash(key);
+
+        const bucket = this.buckets[bucketLocation];
+
+        for (const entry of bucket) {
+          if(entry.key === key) {
+            bucket.pop(entry);
+            return true;
+          }
+        }
+        return false;
+      }
+    
+}
+
+const map = new HashMap();
+
+console.log(map);
+
+console.log(map.hash("manon"));
+
+
+console.log(map.set("sharon", "rebecca"));
+
+console.log(map.get("sharon"));
+console.log(map.has("sharon"));
