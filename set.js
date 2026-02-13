14c97baacff55808892ef7a80978942d1d7dc16e
beckyshaz <sharonsakah8@gmail.com>
2026-02-13 12:40:50 +0300

    created a set that takes key hashes it and adds it to a set of keys in an array, the key can also be removed by a remove method and a clear method that removes all the keys in the array and also a resize method that resizes the capacity to twice its size when number of entries or keys are greater than the load factor which is 0.75,  the capacity is doubled

--- set.js
+++ set.js
@@ -53,9 +53,105 @@ class HashSet{
             }
         }
     }
+
+    //has(key) takes a key as an argument and returns true or false based on whether or not the key is in the hash set.
+
+    has(key) {
+
+        let index = this.hash(key);
+
+        const bucket = this.buckets[index];
+        
+        for (const entry of bucket) {
+            if (entry === key) {
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    //remove(key) takes a key as an argument. If the given key is in the set map, 
+    // it should remove the entry with that key and return true.
+    // If the key isn’t in the set map, it should return false.
+
+
+    remove(key) {
+        const index = this.hash(key);
+
+        const bucket = this.buckets[index];
+
+        for(let i = 0; i < bucket.length; i++){
+            if (bucket[i]=== key) {
+                bucket.splice(i, 1);
+                this.size--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    //length() returns the number of stored keys in the hash map.
+
+    length() {
+        return this.size;
+
+        //without size property
+
+        /*let count = 0;
+
+        for (bucket of this.buckets) {
+            count += bucket.length;
+        }
+        return count;*/
+    }
+
+    //clear() removes all entries in the hash map.
+
+    clear() {
+        //with for of
+
+        /*for (const bucket of this.buckets) {
+            bucket.length = 0;
+        }*/
+
+        for (let i = 0; i < this.buckets.length; i++) {
+            this.buckets[i] = [];
+        }
+        this.size = 0;
+    }
+
+    // keys() returns an array containing all the keys inside the hash map.
+
+    keys() {
+
+        const setKeyArray = []
+
+        for (const bucket of this.buckets) {
+            for (const entry of bucket) {
+                setKeyArray.push(entry);
+
+            }
+        }
+        return setKeyArray;
+    }
+
 }
    
 const newSet = new HashSet();
 console.log(newSet);
 
 console.log(newSet.set("shaz"));
+
+newSet.set("maize");
+
+newSet.set("bena");
+newSet.set("beans");
+newSet.set("groundnut");
+
+console.log(newSet.keys());
+
+console.log(newSet.remove("bena"));
+
+console.log(newSet.keys());
